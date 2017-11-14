59ff3008c81cfae6d5aba94f50883a6e9f9efc8f
thra <thierry.raimond@wanadoo.fr>
2017-11-14 10:08:53 +0100

    Initialisation du projet

---
+++ README.md
@@ -0,0 +1,3 @@
+# Mon-appli-tests-unitaires
+
+Projet Maven Java d'entrainement sur les tests unitaires
---
+++ src/main/java/org/exemple/demo/mon_appli_tests_unitaires/App.java
@@ -0,0 +1,13 @@
+package org.exemple.demo.mon_appli_tests_unitaires;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
